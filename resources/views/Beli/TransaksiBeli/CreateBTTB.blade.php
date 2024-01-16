1ea0a5f4db0214189168acb6df0c2e930ed3e7da
chelsyalfiraa <chelsyyalfira@gmail.com>
2024-01-16 15:59:59 +0700

    Progres 16 Januari 2024
    untuk halaman create dan retur BTTB sudah selesai tp untuk responsifnya masih belum

--- resources/views/Beli/TransaksiBeli/CreateBTTB.blade.php
+++ resources/views/Beli/TransaksiBeli/CreateBTTB.blade.php
@@ -16,12 +16,185 @@
                     </div>
                 @endif
                 <div class="card">
-                    <div class="card-header">Create BTTB</div>
+                    <div class="card-header"><strong>Create BTTB</strong></div>
                     <div class="card-body RDZOverflow RDZMobilePaddingLR0">
-
+                        <div class="row">
+                            <div class="col-md-3 mb-2">
+                                <label for="nolabel" class="form-label"><strong>No. BTTB</strong></label>
+                                <input type="text" class="form-control" id="nolabel" name="nolabel">
+                            </div>
+                            <div class="col-md-3 mb-2">
+                                <label for="nosj" class="form-label"><strong>No. SJ</strong></label>
+                                <input type="text" class="form-control" id="nosj" name="nosj">
+                            </div>
+                            <div class="col-md-3 mb-2">
+                                <label for="sppb" class="form-label"><strong>No. SPPB BC</strong></label>
+                                <input type="text" class="form-control" id="sppb" name="sppb">
+                            </div>
+                            <div class="col-md-3 mb-2">
+                                <label for="supplier" class="form-label"><strong>No. Registrasi</strong></label>
+                                <input type="text" class="form-control" id="supplier" name="supplier">
+                            </div>
+                            <div class="col-md-3 mb-2">
+                                <label for="nomor_po" class="form-label"><strong>tgl BTTB</strong></label>
+                                <input type="date" class="form-control" id="nomor_po" name="nomor_po">
+                            </div>
+                            <div class="col-md-3 mb-2">
+                                <label for="tanggal_po" class="form-label"><strong>No. PIB</strong></label>
+                                <input type="text" class="form-control" id="tanggal_po" name="tanggal_po">
+                            </div>
+                            <div class="col-md-3 mb-2">
+                                <label for="supplier" class="form-label"><strong>Tgl. SPPB BC</strong></label>
+                                <input type="date" class="form-control" id="supplier" name="supplier">
+                            </div>
+                            <div class="col-md-3 mb-2">
+                                <label for="supplier" class="form-label"><strong>Tgl. Rgistrasi</strong></label>
+                                <input type="date" class="form-control" id="supplier" name="supplier">
+                            </div>
+                        <div class="col-md-3 mb-2">
+                            <label for="nomor_po" class="form-label"><strong>Supplier</strong></label>
+                            <input type="text" class="form-control" id="nomor_po" name="nomor_po">
+                        </div>
+                        <div class="col-md-3 mb-2">
+                            <label for="tanggal_po" class="form-label"><strong>No. PIB Ext</strong></label>
+                            <input type="float" class="form-control" id="tanggal_po" name="tanggal_po">
+                        </div>
+                        <div class="col-md-3 mb-2">
+                            <label for="supplier" class="form-label"><strong>No SKBM</strong></label>
+                            <input type="float" class="form-control" id="supplier" name="supplier">
+                        </div>
+                        <div class="col-md-3 mb-2">
+                            <label for="supplier" class="form-label"><strong>Kode HS</strong></label>
+                            <input type="float" class="form-control" id="supplier" name="supplier">
+                        </div>
+                    <div class="col-md-3">
+                        <label for="tanggal_po" class="form-label"><strong>No. PO</strong></label>
+                        <input type="text" class="form-control" id="tanggal_po" name="tanggal_po">
                     </div>
+                    <div class="col-md-3">
+                        <label for="supplier" class="form-label"><strong>Tgl PIB</strong></label>
+                        <input type="date" class="form-control" id="supplier" name="supplier">
+                    </div>
+                    <div class="col-md-3">
+                        <label for="supplier" class="form-label"><strong>Tgl. SKBM</strong></label>
+                        <input type="float" class="form-control" id="supplier" name="supplier">
+                    </div>
+                    <div class="mb-10 m-5">
+                            <div class="table-responsive">
+                        <table class="mx-auto w-750 table sm">
+                            <thead class="table-dark">
+                                <tr>
+                                    <th>No Order</th>
+                                    <th>Kd Barang</th>
+                                    <th style="width: 200px">Nama Barang</th>
+                                    <th>Sub Kategori</th>
+                                    <th>Qty</th>
+                                    <th>Satuan</th>
+                                    <th>Qty Terima</th>
+                                    <th>Qty Shipped</th>
+                                    <th>Qty Remaining</th>
+                                    <th>Harga Unit</th>
+                                    <th>Subtotal</th>
+                                    <th>PPN</th>
+                                    <th>Harga</th>
+                                    <th>Kurs</th>
+                                    <th>IDR Unit</th>
+                                    <th>IDRS</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="row mb-3 m-1">
+                    <div class="col-md-4">
+                        <label for="namabarang" class="form-label"><strong>Nomor Order</strong></label>
+                        <input type="text" class="form-control" id="namabarang" name="namabarang">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="type" class="form-label"><strong>Kurs</strong></label>
+                        <input type="text" class="form-control" id="type" name="type">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="kelompok" class="form-label"><strong>Mata Uang</strong></label>
+                        <input type="text" class="form-control" id="kelompok" name="payment_term">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="namabarang" class="form-label"><strong>Kd. Barang</strong></label>
+                        <input type="text" class="form-control" id="namabarang" name="namabarang">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="type" class="form-label"><strong>Harga Unit</strong></label>
+                        <input type="text" class="form-control" id="type" name="type">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="kelompok" class="form-label"><strong>IDR Unit</strong></label>
+                        <input type="text" class="form-control" id="kelompok" name="payment_term">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="namabarang" class="form-label"><strong>Nama Barang</strong></label>
+                        <input type="text" class="form-control" id="namabarang" name="namabarang">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="type" class="form-label"><strong>Discount</strong></label>
+                        <input type="text" class="form-control" id="type" name="type">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="kelompok" class="form-label"><strong>IDR Discount</strong></label>
+                        <input type="text" class="form-control" id="kelompok" name="payment_term">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="namabarang" class="form-label"><strong>Sub Kategori</strong></label>
+                        <input type="text" class="form-control" id="namabarang" name="namabarang">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="type" class="form-label"><strong>Harga Sub Total</strong></label>
+                        <input type="text" class="form-control" id="type" name="type">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="kelompok" class="form-label"><strong>IDR SubTotal</strong></label>
+                        <input type="text" class="form-control" id="kelompok" name="payment_term">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="namabarang" class="form-label"><strong>Qty ordered</strong></label>
+                        <input type="text" class="form-control" id="namabarang" name="namabarang">
+                    </div>
+                    <div class="col-md-2">
+                        <label for="type" class="form-label"><strong>Qty Remaining</strong></label>
+                        <input type="text" class="form-control" id="type" name="type">
+                    </div>
+                    <div class="col-md-2">
+                        <label for="kelompok" class="form-label"><strong>Harga Total</strong></label>
+                        <input type="text" class="form-control" id="kelompok" name="payment_term">
+                    </div>
+                    <div class="col-md-2">
+                        <label for="kelompok" class="form-label"><strong>IDR Total</strong></label>
+                        <input type="text" class="form-control" id="kelompok" name="payment_term">
+
                 </div>
             </div>
         </div>
-        <script src="{{ asset('js/OrderPembelian/CreateBTTB.js') }}"></script>
-    @endsection
+    </div>
+    </div>
+    </div>
+    <script src="{{ asset('js/OrderPembelian/CreateBTTB.js') }}"></script>
+@endsection
--- resources/views/Beli/TransaksiBeli/ReturBTTB.blade.php
+++ resources/views/Beli/TransaksiBeli/ReturBTTB.blade.php
@@ -5,7 +5,7 @@
 
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-md-10 RDZMobilePaddingLR0">
+            <div class="col-md-18 RDZMobilePaddingLR0">
                 @if (Session::has('success'))
                     <div class="alert alert-success">
                         {{ Session::get('success') }}
@@ -18,10 +18,210 @@
                 <div class="card">
                     <div class="card-header">Retur BTTB</div>
                     <div class="card-body RDZOverflow RDZMobilePaddingLR0">
-
+                        <div class="row mb-3 m-1">
+                            <div class="col-md-4">
+                                <label for="nomor_po" class="form-label">No PO</label>
+                                <input type="text" class="form-control" id="nomor_po" name="nomor_po">
+                            </div>
+                            <div class="col-md-4">
+                                <label for="tanggal_po" class="form-label">Suplier</label>
+                                <input type="text" class="form-control" id="tanggal_po" name="tanggal_po">
+                            </div>
+                            <div class="col-md-4">
+                                <label for="supplier" class="form-label">Payment Term</label>
+                                <input type="text" class="form-control" id="supplier" name="supplier">
+                            </div>
+                        </div>
+                        <div class="row mb-3 m-1">
+                            <div class="col-md-4">
+                                <label for="tanggal_mohonKirim" class="form-label">Tanggal PO</label>
+                                <input type="date" class="form-control" id="tanggal_mohonKirim"
+                                    name="tanggal_mohonKirim">
+                            </div>
+                            <div class="col-md-4">
+                                <label for="payment_term" class="form-label">Tanggal Mohon Kirim</label>
+                                <input type="date" class="form-control" id="payment_term" name="payment_term">
+                            </div>
+                            <div class="col-md-4">
+                                <label for="matauang_input" class="form-label">Mata uang</label>
+                                <input type="text" class="form-control" id="matauang_input" name="matauang_input">
+                            </div>
+                        </div>
                     </div>
+                    <div class="mb-20 m-4">
+                        <table class="mx-auto w-90 table sm">
+                            <thead class="table-dark">
+                                <tr>
+                                    <th>No BTTB</th>
+                                    <th>No. SJ</th>
+                                    <th>Id Terima</th>
+                                    <th>Kd Barang</th>
+                                    <th>Nama Barang</th>
+                                    <th>Sub Kategori</th>
+                                    <th>Qty Terima</th>
+                                    <th>Satuan</th>
+                                    <th>id. Transfer</th>
+                                    <th>id Trans INV</th>
+                                    <th>No Order</th>
+                                    <th>Qty Retur</th>
+                                    <th>Alasan Retur</th>
+                                    <th>Penagih</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        </table>
+                        <table class="table sm">
+                            <thead class="table-dark">
+                                <tr>
+                                    <th>No BTTB</th>
+                                    <th>No. SJ</th>
+                                    <th>Id Terima</th>
+                                    <th>Kd Barang</th>
+                                    <th>Nama Barang</th>
+                                    <th>Sub Kategori</th>
+                                    <th>Qty Terima</th>
+                                    <th>Satuan</th>
+                                    <th>id. Transfer</th>
+                                    <th>id Trans INV</th>
+                                    <th>No Order</th>
+                                    <th>Qty Retur</th>
+                                    <th>Alasan Retur</th>
+                                    <th>Penagih</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <div class="text-left mt-20">
+                                    <h5>Stock Inventory Divisi Pembelian</h5>
+                                </div>
+                                <tr>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                    <td contenteditable="true"></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="row mb-3 m-1">
+                        <div class="col-md-4">
+                            <label for="kdbarang" class="form-label">Kode Barang</label>
+                            <input type="text" class="form-control" id="kdbarang" name="kdbarang">
+                        </div>
+                        <div class="col-md-4">
+                            <label for="tanggalretur" class="form-label">Tanggal Retur</label>
+                            <input type="date" class="form-control" id="tanggalretur" name="tanggalretur">
+                        </div>
+                    </div>
+                    <div class="row mb-3 m-1">
+                        <div class="col-md-4">
+                            <label for="namabarang" class="form-label">Nama Barang</label>
+                            <input type="text" class="form-control" id="namabarang" name="namabarang">
+                        </div>
+                        <div class="col-md-4">
+                            <label for="type" class="form-label">Id Type</label>
+                            <input type="text" class="form-control" id="type" name="type">
+                        </div>
+                        <div class="col-md-4">
+                            <label for="kelompok" class="form-label">Kelompok</label>
+                            <input type="text" class="form-control" id="kelompok" name="payment_term">
+                        </div>
+                        <div class="col-md-4">
+                            <label for="namabarang" class="form-label">Sub Kategori</label>
+                            <input type="text" class="form-control" id="namabarang" name="namabarang">
+                        </div>
+                        <div class="col-md-2">
+                            <label for="type" class="form-label">Qty Retur Primer</label>
+                            <input type="float" class="form-control" id="type" name="type">
+                        </div>
+                        <div class="col-md-2">
+                            <label for="kelompok" class="form-label">Qty Sekunder</label>
+                            <input type="float" class="form-control" id="kelompok" name="payment_term">
+                        </div>
+                        <div class="col-md-2">
+                            <label for="kelompok" class="form-label">Qty Tertier</label>
+                            <input type="float" class="form-control" id="kelompok" name="payment_term">
+                        </div>
+                    </div>
+                    <div class="row mb-3 m-1">
+                        <div class="col-md-2">
+                            <label for="kelompok" class="form-label">No. BTTB</label>
+                            <input type="float" class="form-control" ids="kelompok" name="payment_term">
+                        </div>
+                        <div class="col-md-8" style="width: 400px; margin-right: -60px;">
+                            <label for="kelompok" class="form-label">Alasan</label>
+                            <input type="float" class="form-control" id="kelompok" name="payment_term">
+                        </div>
+                    </div>
+
+                    <div class="row mb-3 m-1">
+                        <div class="col-md-2">
+                            <label for="kelompok" class="form-label">No. SJ</label>
+                            <input type="float" class="form-control" id="kelompok" name="payment_term">
+                        </div>
+                    </div>
+                    <div class="row mb-3 m-1">
+                        <div class="col-md-2">
+                            <label for="kelompok" class="form-label">Id Terima</label>
+                            <input type="int" class="form-control" id="kelompok" name="payment_term">
+                        </div>
+                    </div>
+                    <div class="row mb-3 m-1">
+                        <div class="col-md-2">
+                            <label for="kelompok" class="form-label">Qty Terima</label>
+                            <input type="float" class="form-control" id="kelompok" name="payment_term">
+                        </div>
+                    </div>
+
+                    <!-- Buttons -->
+                    <div class="row align-items-center">
+                        <div class="col-md-12 d-flex justify-content-end">
+                        <button type="button" class="btn  btn-lg btn-success mr-3">POST</button>
+                        <button type="button" class="btn btn-danger">EXIT</button>
+                        </div>
+
                 </div>
+
             </div>
         </div>
-        <script src="{{ asset('js/OrderPembelian/ReturBTTB.js') }}"></script>
+        <script src="{{ asset('js/OrderPembelian/ListPurchaseOrder.js') }}"></script>
+        <script>
+            function printData() {
+                // Add your print logic here
+                console.log('Printing...');
+            }
+
+            function exitPage() {
+                // Add your exit logic here
+                console.log('Exiting...');
+            } <
+            script src = "{{ asset('js/OrderPembelian/ReturBTTB.js') }}" >
+        </script>
     @endsection
