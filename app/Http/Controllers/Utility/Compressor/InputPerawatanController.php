c5ac07fe79e997b7415294aade6861d0f138d11f
Rayhan Rizaldi Hi Hukum <rayhanrizaldi.rr@gmail.com>
2024-03-07 10:03:54 +0700

    Update InputPerawatanController.php
    fix list teknisi

--- app/Http/Controllers/Utility/Compressor/InputPerawatanController.php
+++ app/Http/Controllers/Utility/Compressor/InputPerawatanController.php
@@ -14,12 +14,22 @@ class InputPerawatanController extends Controller
     {
         $mesin = DB::connection('ConnUtility')->select('exec SP_LIST_MESIN_COMPRESSOR');
         $part = DB::connection('ConnUtility')->select('exec SP_LIST_PART_COMPRESSOR');
-        $teknisi = DB::connection('ConnUtility')->select('exec SP_LIST_UTILITY_TEKNISI');
+        $IDUser = auth::user()->IDUser;
+        $lokasiResult = DB::connection('ConnUtility')->table('Utility_Teknisi')
+            ->select('Lokasi')
+            ->where('IdUserMaster', $IDUser)
+            ->first();
+
+        $lokasi = $lokasiResult ? $lokasiResult->Lokasi : null;
+
+        $teknisi = DB::connection('ConnUtility')
+            ->select("exec SP_LIST_UTILITY_TEKNISI @lokasi = ?", [$lokasi]);
 
         $access = (new HakAksesController)->HakAksesFiturMaster('Utility');
 
         return view('Utility.Compressor.InputPerawatan.index', compact('mesin', 'part', 'teknisi', 'access'));
     }
+
 
     public function getKeterangan(Request $request)
     {
